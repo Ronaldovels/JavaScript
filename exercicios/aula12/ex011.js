7aec2b8de0f2eb3411f18f2cb778d4cba3d9449c
Ronaldo <ronaldovf24@gmail.com>
2022-12-05 21:06:49 -0300

    a

--- exercicios/aula12/ex011.js
+++
@@ -1,8 +0,0 @@
-var idade = 65
-if (idade < 16) {
-    console.log('Não vota')
-} else if (idade < 1 || idade >= 65) {
-        console.log('Voto opcional')
-} else {
-        console.log('Voto obrigatorio')
-}
